281a4b43c842e947858d836b9c9393bc96d87bd8
GtrCraft <nickvanbokhorst@hotmail.com>
2021-04-28 09:44:58 +0200

    surya: Time to kang

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_surya.mk
+    $(LOCAL_DIR)/kangos_surya.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_surya-user \
-    lineage_surya-userdebug \
-    lineage_surya-eng
+    kangos_surya-user \
+    kangos_surya-userdebug \
+    kangos_surya-eng
--- kangos_surya.mk
+++ kangos_surya.mk
@@ -21,14 +21,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 # Inherit from surya device
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Kang stuff.
+$(call inherit-product, vendor/kangos/config/common.mk)
+
+# Kangos official
+KANGOS_BUILDTYPE := OFFICIAL
+
+# Kangos Maintainer
+PRODUCT_PRODUCT_PROPERTIES += \
+  ro.kangos.maintainer=Nick van Bokhorst (GtrCraft) \
+  ro.kangos.cpu=SDM732
 
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
 
 # Device identifier
-PRODUCT_NAME := lineage_surya
+PRODUCT_NAME := kangos_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO X3
